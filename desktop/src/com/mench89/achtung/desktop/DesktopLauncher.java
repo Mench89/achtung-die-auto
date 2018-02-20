1c5847c73500fb8788d81ec62c06f68db55c30d3
Mench89 <magnus.lindberg89@gmail.com>
2018-02-20 22:03:23 +0100

    Initial project setup
    * Now possible to build jar!
    Build by:
    ./gradlew desktop:dist
    located in desktop/build/libs
    
    Change-Id: I986c71825910900021e6147bbc01672942376ac8

---
+++ desktop/src/com/mench89/achtung/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mench89.achtung.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mench89.achtung.AchtungGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AchtungGame(), config);
+	}
+}
